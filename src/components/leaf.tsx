de2240f0e4dc55a96cddaeda3ef93c16a08d82e6
Apelpollo <alanerives19@gmail.com>
2024-06-04 12:29:23 -0600

    int

--- src/components/leaf.tsx
+++ src/components/leaf.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { leaf, leafWrapper } from "../../variants";
 
-const Leaf = ({  className = "", imageUrl = "", animationSpeed = "1" }) => {
+const Leaf = ({  className = "", imageUrl = "", animationSpeed = 1.8 }) => {
   return (
     <motion.div variants={leafWrapper} className={className}>
       <motion.img custom={animationSpeed} variants={leaf} className="leaf" src={imageUrl} />
